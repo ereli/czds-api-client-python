b5de3c33d6334a72987bf5348c16862209806f6d
Ereli Eran <ereli.eran@gmail.com>
2022-12-05 19:08:37 -0500

    wup

--- setup.py
+++ setup.py
@@ -2,5 +2,8 @@ from setuptools import find_packages, setup
 
 setup(name='czds_api_client',
       version='0.1.0',
-      packages=find_packages(),
-     )
+      description='Python client for the CZDS API',
+      packages=find_packages("src"),
+      package_dir={"": "src"},
+      provides=["czds_api_client"],
+)
